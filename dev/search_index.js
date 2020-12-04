f0fb638b89d54bb3d63fffc3419bb868220b7c0e
Documenter.jl <documenter@juliadocs.github.io>
2020-12-04 22:57:22 +0000

    build based on 5350855

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MathOptPresolve","category":"page"},{"location":"#MathOptPresolve","page":"Home","title":"MathOptPresolve","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MathOptPresolve]","category":"page"}]
+}
