7ac1433a2607246d816087816457123c1ef9ca8c
BochuanBob <lub@rose-hulman.edu>
2021-01-09 17:39:28 -0600

    Updated util.jl

--- src/mip/round_integer_bounds.jl
+++ src/mip/round_integer_bounds.jl
@@ -1,21 +1,3 @@
-# Allow some numerical errors to avoid the situation
-# like ceiling 1e-10 to 1.
-function approx_ceil(val::T, ϵ_int::T)::T where{T}
-    if (val - floor(val)) < ϵ_int
-        return T(floor(val))
-    end
-    return T(ceil(val))
-end
-
-# Allow some numerical errors to avoid the situation
-# like flooring 1 - 1e-10 to 0.
-function approx_floor(val::T, ϵ_int::T)::T where{T}
-    if (ceil(val) - val) < ϵ_int
-        return T(ceil(val))
-    end
-    return T(floor(val))
-end
-
 function round_integer_bounds!(ps::PresolveData{T}, j::Int, ϵ_int::T=eps(T)) where{T}
     # Column was already removed or the variable is continous.
     ps.colflag[j] || return nothing
